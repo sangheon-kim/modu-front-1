5f3f4cca20d1864e5ae1e7a439d7f8483f76a90b
peppone-choi <peppone.choi@gmail.com>
2024-08-13 20:08:12 +0900

    [최병호] 주석 추가

--- vanilla-movie/src/api/@types/tmdb/nowPlayingTv.type.ts
+++ vanilla-movie/src/api/@types/tmdb/nowPlayingTv.type.ts
@@ -21,25 +21,43 @@ export type getNowPlayingTVRequest = {
 };
 
 export type getNowPlayingTVResponse = {
+  /** 페이지 */
   page: number;
+  /** 데이터 */
   results: Array<getNowPlayingTVResponseData>;
+  /** 총 페이지 수 */
   total_pages: number;
+  /** 총 결과 수 */
   total_results: number;
 };
 
 export type getNowPlayingTVResponseData = {
+  /** 성인 컨텐츠 포함 여부 */
   adult: boolean;
+  /** 배경 이미지 */
   backdrop_path: string;
+  /** 장르 */
   genre_ids: Array<number>;
+  /** 아이디 */
   id: number;
+  /** 방영 국가 */
   origin_country: Array<string>;
+  /** 방영 언어 */
   original_language: string;
+  /** 원래 이름 */
   original_name: string;
+  /** 소개 */
   overview: string;
+  /** 인기도 */
   popularity: number;
+  /** 포스터 이미지 */
   poster_path: string;
+  /** 첫 방영일 */
   first_air_date: string;
+  /** 제목 */
   name: string;
+  /** 평점 */
   vote_average: number;
+  /** 투표 수 */
   vote_count: number;
 };
