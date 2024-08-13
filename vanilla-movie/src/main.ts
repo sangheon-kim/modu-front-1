c17ebf6267506068d5119c1eac2ac68b1d3209e5
peppone-choi <peppone.choi@gmail.com>
2024-08-13 19:28:23 +0900

    [최병호] 경기도 부천시 지하철7호선 부천구간 역별 이용객수 조회 API 작성

--- vanilla-movie/src/main.ts
+++ vanilla-movie/src/main.ts
@@ -4,28 +4,37 @@ import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
 import { WeatherService } from "./api/services/weathers/weather.service.ts";
+import { PDataService } from "./api/services/PData/index.service.ts";
 
 const movieListService = new MovieListService();
 const weatherService = new WeatherService();
+const pDataService = new PDataService();
 
 (async () => {
-  const data = await movieListService.getPopularMovies({
-    params: {
-      include_adult: false,
-      include_video: false,
-      language: "ko-KR",
-      page: 1,
-      sort_by: "popularity.desc",
-    },
-  });
-
-  const weatherData = await weatherService.getCurrentWeather({
-    params: {
-      lat: 37.5665,
-      lon: 126.978,
-    },
-  });
-
+  // const data = await movieListService.getPopularMovies({
+  //   params: {
+  //     include_adult: false,
+  //     include_video: false,
+  //     language: "ko-KR",
+  //     page: 1,
+  //     sort_by: "popularity.desc",
+  //   },
+  // });
+  // const weatherData = await weatherService.getCurrentWeather({
+  //   params: {
+  //     lat: 37.5665,
+  //     lon: 126.978,
+  //   },
+  // });
+  // const buchonSubwayPassengerData = await pDataService.getBucheonSubwayPassenger({
+  //   params: {
+  //     page: 1,
+  //     perPage: 10,
+  //     returnType: "json",
+  //   },
+  // });
+  // console.log("buchonSubwayPassengerData", buchonSubwayPassengerData);
+  // console.log("buchonSubwayPassengerData", buchonSubwayPassengerData);
   // await console.log("page", data.page);
 })();
 
