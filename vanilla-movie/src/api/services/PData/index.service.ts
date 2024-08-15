090726ab95a9043116c559573dc842d47959c06f
ehddk <bj9147@naver.com>
2024-08-15 21:01:48 +0900

    API 작성

--- vanilla-movie/src/api/services/PData/index.service.ts
+++ vanilla-movie/src/api/services/PData/index.service.ts
@@ -1,4 +1,48 @@
+import { getSeoulRestaurantRequest ,getSeoulRestaurantResponse} from "../../@types/PData/seoulRestaurant.type";
+
+const API_KEY = "hMojIvvHF%2B09wM6EWKKRIKhgicN%2FZohDT9YErSSMrpz76XeTM0XzQZi7x6L2bE1qa%2Bw24QV232F0GrcA2aSiZQ%3D%3D"
 export class PDataService {
   /** 미세먼지 경보 발령 현황 조회 */
   async getFineDustAlarm() {}
+    /** 서울특별시 서초구 음식점 조회 */
+  async getSeoulRestaurant(req:getSeoulRestaurantRequest){
+    const{params} =req;
+    const url=new URL(`https://api.odcloud.kr/api/3053840/v1/uddi:9cb85e53-3592-4a8d-9b07-eec77edbc8c2`)
+
+    Object.entries({
+      ...params,
+      serviceKey: decodeURIComponent(API_KEY),
+    }).forEach(([key, value]) => {
+      url.searchParams.append(key, value.toString());
+    });
+
+    const res=await fetch(url);
+    const json = await res.json();
+    //console.log('json',json)
+    const data = json.data.map((ele:any) => {
+       return{
+        //업소명
+        upsoName:  ele["업소명"],
+        //소재지도로명주소
+        roadAddr :  ele["소재지도로명주소"],
+        //소재지지번주소
+        locationAddr:  ele["소재지지번주소"],
+        //업태명 
+        businessName:  ele["업태명"],
+        //영업장면적 
+         businessArea: ele["영업장면적 "],
+       }
+       
+    });
+    const response:getSeoulRestaurantResponse={
+      page:json.page,
+      perPage:json.perPage,
+      totalCount:json.totalCount,
+      matchCount:json.matchCount,
+      currentCount:json.currentCount,
+      data:data,
+
+    };
+    return response;
+  }
 }
--- vanilla-movie/src/main.ts
+++ vanilla-movie/src/main.ts
@@ -4,47 +4,54 @@ import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
 import { WeatherService } from "./api/services/weathers/weather.service.ts";
+import { TVListService } from "./api/services/tmdb/tvList.service.ts";
+import { PDataService } from "./api/services/PData/index.service.ts";
 
 const movieListService = new MovieListService();
 const weatherService = new WeatherService();
+const tvListService = new TVListService();
+const pDataService = new PDataService();
+const forecastService=new WeatherService();
 
 (async () => {
-  const data = await movieListService.getPopularMovies({
-    params: {
-      include_adult: false,
-      include_video: false,
-      language: "ko-KR",
-      page: 1,
-      sort_by: "popularity.desc",
-    },
-  });
+    // const data = await movieListService.getPopularMovies({
+    //   params: {
+    //     include_adult: false,
+    //     include_video: false,
+    //     language: "ko-KR",
+    //     page: 1,
+    //     sort_by: "popularity.desc",
+    //   },
+    // });
 
-  const weatherData = await weatherService.getCurrentWeather({
-    params: {
-      lat: 37.5665,
-      lon: 126.978,
-    },
-  });
+    // const weatherData = await weatherService.getCurrentWeather({
+    //   params: {
+    //     lat: 37.5665,
+    //     lon: 126.978,
+    //   },
 
-  // await console.log("page", data.page);
+    /** 서울 서초구 음식점 */
+    const restaurantData = await pDataService.getSeoulRestaurant({
+      params: {
+        page: 1,
+        perPage: 10,
+      },
+    });
+    /**변경 tv 리스트 목록 */
+    const TVdata = await tvListService.getChangeTV({
+      params: {
+        page: 1,
+      },
+    });
+  /*일상 예측 16일*/
+    const dailyForecastData=await forecastService.getForeCast16({
+      params:{
+        lat:37.6634779,
+        lon:127.2495274,
+        cnt:3
+      }
+    })
+    console.log('restaurantData: ', restaurantData)
+    console.log('tvData',TVdata)
+    console.log('forecastData',dailyForecastData);
 })();
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
