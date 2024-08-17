abdd7f863fa8d0f7e33741b5146106461ddbb708
cherry2407 <codus7958@gmail.com>
2024-08-17 16:48:29 +0900

    재난문자발령현황 서비스함수 수정/트렌드 무비 타입정의,서비스함수 완료

--- vanilla-movie/src/api/services/PData/disasterMsg.service.ts
+++ vanilla-movie/src/api/services/PData/disasterMsg.service.ts
@@ -10,7 +10,6 @@ export class DisasterMsgservice {
   async getDisasterMsg(req: getDisasterMsgRequest) {
     const url = new URL(`${HOST_URL}/1741000/DisasterMsg5/getDisasterMsg5List`);
 
-    // URL 뒤에 ?쿼리 뒤에 붙여줄 파라미터들을 추가
     for (let [key, value] of Object.entries(req.params)) {
       url.searchParams.append(key, value.toString());
     }
